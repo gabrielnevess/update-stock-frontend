facc9c8f693b93afe2781d2a39192fa1d2ced2f3
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-11-20 01:14:38 -0300

    logoff ao expirar sessão

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,8 +8,23 @@ import PublicRoute from "./PublicRoute";
 import Sidebar from "../pages/Inventory/Sidebar";
 import Login from "../pages/Auth/Login";
 import PrivateRoute from "./PrivateRoute";
+import { AxiosError, AxiosResponse } from "axios";
+import { useAuth } from "../hooks/auth";
+import api from "../services/api";
 
 const Routes: React.FC = () => {
+    const { signOut } = useAuth();
+
+    api.interceptors.response.use(function (response: AxiosResponse<any>) {
+        return response;
+    }, function (error: AxiosError<any>) {
+        if (error?.response?.status === 401) {
+            signOut();
+        } else {
+            return Promise.reject(error);
+        }
+    });
+
     return (
         <Switch>
 
